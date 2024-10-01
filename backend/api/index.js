c11b440cdf27501a9a5eeccf03b971e734fe0b73
Syed Muhammad Danish <danishshah2203651@gmail.com>
2024-10-01 04:15:51 -0700

    updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,11 +2,11 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js";
-import messageRoute from "./routes/message.route.js";
-import { app, server } from "./socket/socket.js";
+import connectDB from "../utils/db.js";
+import userRoute from "../routes/user.route.js";
+import postRoute from "../routes/post.route.js";
+import messageRoute from "../routes/message.route.js";
+import { app, server } from "../socket/socket.js";
 import path from "path";
 
 dotenv.config();
